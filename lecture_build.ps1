9ef8c59f1f5198023c076ec286c38551403fb532
moriees25 <moriees0473@gmail.com>
2026-04-07 05:58:42 +0900

    2nd

--- lecture_build.ps1
+++ lecture_build.ps1
@@ -4,8 +4,13 @@
 # npm install @slidev/cli@0.49.29
 # npx @slidev/cli --version     52.14.2
 
+# git add .
+# git commit -m "fix permission 
+# git push origin main
+
+
 # http://localhost:3030/
 
 Remove-Item -Recurse -Force node_modules\.vite -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 1
 npx slidev --port 3030 --open
